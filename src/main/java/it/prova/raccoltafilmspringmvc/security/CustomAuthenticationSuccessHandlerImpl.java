ade56b99411cd7f40dae9672f86f54377676ffe7
amultari <a.multari@solvingteam.it>
2022-03-21 14:04:00 +0100

    Utilizzato DTO per info in sessione al posto di Utente entity

--- src/main/java/it/prova/raccoltafilmspringmvc/security/CustomAuthenticationSuccessHandlerImpl.java
+++ src/main/java/it/prova/raccoltafilmspringmvc/security/CustomAuthenticationSuccessHandlerImpl.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import it.prova.raccoltafilmspringmvc.dto.UtenteDTO;
 import it.prova.raccoltafilmspringmvc.model.Utente;
 import it.prova.raccoltafilmspringmvc.repository.utente.UtenteRepository;
 
@@ -27,7 +28,7 @@ public class CustomAuthenticationSuccessHandlerImpl implements AuthenticationSuc
 		
 		//voglio mettere in sessione uno userInfo perché spring security mette solo un principal da cui attingere username
 		Utente utenteFromDb = utenteRepository.findByUsername(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException("Username " + authentication.getName() + " not found"));
-		Utente utenteParziale = new Utente();
+		UtenteDTO utenteParziale = new UtenteDTO();
 		utenteParziale.setNome(utenteFromDb.getNome());
 		utenteParziale.setCognome(utenteFromDb.getCognome());
 		request.getSession().setAttribute("userInfo", utenteParziale);
